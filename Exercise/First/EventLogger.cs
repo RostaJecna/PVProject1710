33975c83a5247fa85e9dd8990070c6b5f7e21ec2
r0usta <rostyslavl004@gmail.com>
2022-10-22 23:35:31 +0200

    Create List for Event and write method Filter()

--- Exercise/First/EventLogger.cs
+++ Exercise/First/EventLogger.cs
@@ -8,5 +8,42 @@ namespace PVProject1710.Exercise.First
 {
     internal class EventLogger
     {
+        public List<Event> Events { get; set; }
+
+        public EventLogger(List<Event> events)
+        {
+            Events = events;
+        }
+
+        public void AddEvent(Event @event)
+        {
+            Events.Add(@event);
+        }
+
+        public List<Event> Filter(Type type, Level level)
+        {
+            if(type == Type.Any && level == Level.Any)
+            {
+                return Events;
+            }
+
+            List<Event> filter = new();
+
+            foreach (Event @event in Events)
+            {
+                if(@event.Type == type && @event.Level == level)
+                {
+                    filter.Add(@event);
+                }else if (@event.Type == type && level == Level.Any)
+                {
+                    filter.Add(@event);
+                }else if (@event.Type == Type.Any && @event.Level == level)
+                {
+                    filter.Add(@event);
+                }
+            }
+
+            return filter;
+        }
     }
 }
