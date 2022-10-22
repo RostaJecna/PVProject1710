861581e6ca944ba7805ba02ab7869775427f2486
r0usta <rostyslavl004@gmail.com>
2022-10-22 23:34:04 +0200

    Create parameters for class.

--- Exercise/First/Event.cs
+++ Exercise/First/Event.cs
@@ -8,5 +8,20 @@ namespace PVProject1710.Exercise.First
 {
     internal class Event
     {
+        public Type Type { get; set; }
+        public Level Level { get; set; }
+        public string Description { get; set; }
+
+        public Event(Type type, Level level, string description)
+        {
+            Type = type;
+            Level = level;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return "Event: " + Type + "." + Level + "\n[" + Description +"]";
+        }
     }
 }
